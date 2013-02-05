105b7cd79a0492045921fe09e183fe3cd2d04ae0
Fauntleroy <the.kaempfer@gmail.com>
2013-02-05 08:55:38 -0800

    check for null results in url search fixes #53

--- assets/scripts/views/message.js
+++ assets/scripts/views/message.js
@@ -38,7 +38,7 @@ irc.Views.Message = Backbone.View.extend({
 
 			var contents = this.model.get('contents');
 			var url_regex = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/ig;
-			var urls = contents.match( url_regex );
+			var urls = contents.match( url_regex ) || [];
 
 			if( urls.length > 0 ){
 
