64b352998a796ffc691524c360cf39406261e7d2
fauntleroy <the.kaempfer@gmail.com>
2013-02-23 23:34:04 -0800

    emphasize messages that start with user's nick

--- assets/scripts/models/message.js
+++ assets/scripts/models/message.js
@@ -3,8 +3,10 @@ irc.Models.Message = Backbone.Model.extend({
 	initialize: function(){
 
 		var user = this.collection.channel.users.findWhere({ nick: this.get('nick') }); // bad
+		var nick_mention_regex = new RegExp( '^'+ this.collection.connection.get('nick') ); // bad?
 
 		if( user ) this.set( 'presence_id', user.get('presence_id') );
+		if( nick_mention_regex.test( this.get('contents') ) ) this.set( 'mention', true );
 
 	}
 
