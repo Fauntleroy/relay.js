d1c33d7c6624495d7ad766ff59108cc0fc76ec6d
Fauntleroy <the.kaempfer@gmail.com>
2013-11-11 11:31:53 -0800

    remove spaces from handlebars_helpers.js

--- assets/scripts/handlebars_helpers.js
+++ assets/scripts/handlebars_helpers.js
@@ -12,10 +12,10 @@ module.exports = function( Handlebars ){
 	});
 
 	Handlebars.registerHelper( 'breaklines', function( text ){
-	    text = Handlebars.Utils.escapeExpression( text );
-	    text = text.toString();
-	    text = text.replace( /(\r\n|\n|\r)/gm, '<br />' );
-	    return new Handlebars.SafeString( text );
+		text = Handlebars.Utils.escapeExpression( text );
+		text = text.toString();
+		text = text.replace( /(\r\n|\n|\r)/gm, '<br />' );
+		return new Handlebars.SafeString( text );
 	});
 
 	Handlebars.registerHelper( 'encodeURI', function( string ){
